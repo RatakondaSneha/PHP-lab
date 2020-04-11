95a0d52327b2a5266ad617a8fe7c013719394b15
ratakondasneha <ratakondasneha@gmail.com>
2020-04-11 23:08:39 +0000

    lab

--- private/app/views/main/login.php
+++ private/app/views/main/login.php
@@ -7,5 +7,14 @@ form > * {
 
 </style>
 
-<form action = "/user/login" method = 
+<form action = "/user/login" method = "POST">
+<input type = "hidden" name = "csrf" value = "<?php echo($csrf) ?>">
+<label for = "username"> Email </label>
+<input type = "text" id = "username" name = "username" required autocomplete = "Email" > 
+<label for =  "password" > Password </label>
+<input type = "password" id ="password" name="password" autocomplete = "Password" >
+<input type = "submit" value= "Login"> 
+</form>
+
+
 
