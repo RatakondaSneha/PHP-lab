57457e005de3ce882f92db2c257d4eace34a837b
ratakondasneha <ratakondasneha@gmail.com>
2020-04-23 03:58:40 +0000

    final

---
+++ private/app/views/blog/create.php
@@ -0,0 +1,18 @@
+<h1>
+Create Post
+</>
+
+<form method = "post" action = "/blog/create" >
+<input type = "hidden" value = "" name = "csrf">
+<label for = "title">Title</label>
+
+<input type = "text" id = "title" name = "title">
+<label for = "author">Author</label>
+
+<input type = "email" id = "author" name = "author">
+<label for = "content">Content</label>
+
+
+<textarea name = "content" id = "content"></textarea>
+<input type = "submit" value = "submit">
+</form>
