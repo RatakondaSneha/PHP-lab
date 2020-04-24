85164124e04ff7777759ea4ae3b2359857951d77
ratakondasneha <ratakondasneha@gmail.com>
2020-04-24 18:39:28 +0000

    final

--- private/app/controllers/blog.php
+++ private/app/controllers/blog.php
@@ -51,6 +51,38 @@ class  Blog extends Controller
             $this -> view("blog/create");
             $this -> view("template/footer");
 
+
+        }
+    }
+
+    function PostUpdate($postId)
+    {
+        $is_auth = isset($_SESSION["username"]);
+        if(!$is_auth)
+        {
+            header("location: /blog");
+            return;
+        }
+
+        if($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            $slug = $_POST["slug"];
+            $title = $_POST["title"];
+            $content = $_POST["content"];
+            $author = $_POST["author"];
+            $this -> model("BlogModel");
+            $slug = $this -> BlogModel -> updateBlog($slug , $title , $author , $content);
+            header("location: /blog/read/" . $slug);
+        }
+
+        else
+        {
+            $this -> model("BlogModel");
+            $post = $this -> BlogModel -> getPostById($postId);
+            $this -> view("blog/postupdate" , $post);
+            $this -> view("template/footer");
+
+
         }
     }
 }
